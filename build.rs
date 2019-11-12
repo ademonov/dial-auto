44de277dca4960f2728e067287154781c6ee572a
Andrew.Demonov <fcoder@live.com>
2019-11-12 01:09:34 -0500

    add systray

---
+++ build.rs
@@ -0,0 +1,16 @@
+use std::path::Path;
+use std::{env, fs};
+
+//const ICON_ON: &str = "on.ico";
+const ICON_OFF: &str = "off.ico";
+
+fn main() {
+    let target_dir_path = env::var("OUT_DIR").unwrap();
+//    copy(&target_dir_path, ICON_ON);
+    copy(&target_dir_path, ICON_OFF);
+}
+
+fn copy<S: AsRef<std::ffi::OsStr> + ?Sized, P: Copy + AsRef<Path>>(target_dir_path: &S, file_name: P) {
+    let dst_path = Path::new(&target_dir_path).join("../../..").join(file_name);
+    fs::copy(file_name, dst_path).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+// #![windows_subsystem = "windows"]
+
+#[cfg(target_os = "windows")]
+fn main() -> Result<(), systray::Error> {
+    let mut app;
+    match systray::Application::new() {
+        Ok(w) => app = w,
+        Err(_) => panic!("Can't create window!"),
+    }
+    app.set_icon_from_file("off.ico")?;
+
+    app.add_menu_item("Print a thing", |_| {
+        println!("Printing a thing!");
+        Ok::<_, systray::Error>(())
+    })?;
+
+    app.add_menu_item("Add Menu Item", |window| {
+        window.add_menu_item("Interior item", |_| {
+            println!("what");
+            Ok::<_, systray::Error>(())
+        })?;
+        window.add_menu_separator()?;
+        Ok::<_, systray::Error>(())
+    })?;
+
+    app.add_menu_separator()?;
+
+    app.add_menu_item("Quit", |window| {
+        window.quit();
+        Ok::<_, systray::Error>(())
+    })?;
+
+    println!("Waiting on message!");
+    app.wait_for_message()?;
+    Ok(())
 }
+
+ #[cfg(not(target_os = "windows"))]
+ fn main() {
+     panic!("Not implemented on this platform!");
+ }
